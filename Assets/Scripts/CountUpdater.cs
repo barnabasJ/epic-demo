9f05bf521d988a259b018c7c35dce03ce23fb3bc
TheCad <mbeijen1@gmail.com>
2020-03-13 13:38:07 +0100

    Added a event to update the "inventory"

---
+++ Assets/Scripts/CountUpdater.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CountUpdater : MonoBehaviour
+{
+    public static CountUpdater instance;
+    public Text itemCount;
+    
+    public Inventory inventory;
+    // Call this event by using CountUpdater.instance.pickupEvent.Invoke()
+    public UnityEvent pickupEvent;
+
+    private void Awake()
+    {
+        instance = this;
+        pickupEvent = new UnityEvent();
+    }
+
+    void Start()
+    {
+        pickupEvent.AddListener(UpdatePickupAmount);
+    }
+
+    void Update()
+    {
+        itemCount.text = inventory.currentAmount.ToString();
+    }
+
+    void UpdatePickupAmount()
+    {
+        if (inventory.currentAmount < inventory.maxAmount)
+            inventory.currentAmount += 1;
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "inventory", menuName = "ScriptableObjects/Inventory", order = 1)]
+
+public class Inventory : ScriptableObject
+{
+    public int maxAmount;
+    public int currentAmount;
+}
